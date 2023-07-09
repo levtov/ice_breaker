0b1afad6b1f9e222ddfbf87e8c4af3d69eef5a52
Lev Zak <lev.zak@medcurrent.com>
2023-07-08 21:08:45 -0400

    Search for linkedin profile in Google

---
+++ ice_breaker.py
@@ -0,0 +1,37 @@
+# import os
+from third_parties.linkedin import scrape_linkedin_profile
+
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from third_parties.linkedin import scrape_linkedin_profile
+
+
+# information = scrape_linkedin_profile(linkedin_profile_url="doesnt_metter")
+
+if __name__ == "__main__":
+    print("hello LangChain!")
+    # print(os.environ["OPENAI_API_KEY"])
+
+    # linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco Udemy")
+    linkedin_profile_url = linkedin_lookup_agent(name="Zak Lev Medcurrent")
+
+    summary_template = """
+    given the linkedin information {information} about a person from I want you to create:
+    1. a short summary
+    2, two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    print(chain.run(information=linkedin_data))
+
+    # print(linkedin_data.json()["public_identifier"])
---
+++ third_parties/mytest.py
@@ -0,0 +1,10 @@
+# import requests
+
+# api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+# api_key = "4QYeM2GXbbfbXntcFOqxPQ"
+# header_dic = {"Authorization": "Bearer " + api_key}
+# params = {
+#     "url": "https://www.linkedin.com/in/zaklev/",
+# }
+# response = requests.get(api_endpoint, params=params, headers=header_dic)
+# print(response.json())
